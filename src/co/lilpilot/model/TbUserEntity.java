01d46cccb81bb80592d134e353b3e8ef2fa8c5bc
Xuzy <xuzywozz@163.com>
2016-05-23 14:09:00 +0800

    Initial commit

---
+++ src/co/lilpilot/model/TbUserEntity.java
@@ -0,0 +1,105 @@
+package co.lilpilot.model;
+
+import javax.persistence.*;
+
+/**
+ * Created by xuzywozz on 2016/5/23.
+ */
+@Entity
+@Table(name = "tb_user", schema = "myblog", catalog = "")
+public class TbUserEntity {
+    private int userId;
+    private String name;
+    private String password;
+    private String access;
+    private String status;
+    private String remark;
+
+    @Id
+    @Column(name = "user_id", nullable = false)
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    @Basic
+    @Column(name = "name", nullable = false, length = 255)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "password", nullable = false, length = 255)
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Basic
+    @Column(name = "access", nullable = true, length = 255)
+    public String getAccess() {
+        return access;
+    }
+
+    public void setAccess(String access) {
+        this.access = access;
+    }
+
+    @Basic
+    @Column(name = "status", nullable = true, length = 255)
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Basic
+    @Column(name = "remark", nullable = true, length = 255)
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TbUserEntity that = (TbUserEntity) o;
+
+        if (userId != that.userId) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (password != null ? !password.equals(that.password) : that.password != null) return false;
+        if (access != null ? !access.equals(that.access) : that.access != null) return false;
+        if (status != null ? !status.equals(that.status) : that.status != null) return false;
+        if (remark != null ? !remark.equals(that.remark) : that.remark != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = userId;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (access != null ? access.hashCode() : 0);
+        result = 31 * result + (status != null ? status.hashCode() : 0);
+        result = 31 * result + (remark != null ? remark.hashCode() : 0);
+        return result;
+    }
+}
